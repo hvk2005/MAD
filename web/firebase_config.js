a78ccad023e4b42a92a32b0f9314c43476261c05
Harshil063 <164226024+hvk2005@users.noreply.github.com>
2025-04-30 14:13:36 +0530

    Initialization

---
+++ web/firebase_config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  // Your web app's Firebase configuration
+  // Get this from your Firebase Console
+  apiKey: "AIzaSyCzdrHsnQfn9OtExDKOFXpeQd9VcMIIYWo",
+  authDomain: "madhk-55157.firebaseapp.com",
+  projectId: "madhk-55157",
+  storageBucket: "madhk-55157.firebasestorage.app",
+  messagingSenderId: "115124468211",
+  appId: "1:115124468211:web:aa65850d320321d9403a86",
+  measurementId: "G-VDTR8S0NJW",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
